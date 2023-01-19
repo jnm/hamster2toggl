10348dc74e28dd68f36b9d0c78cd90b51cee35a2
John N. Milner <john@tmoj.net>
2023-01-19 15:36:04 -0500

    Read only projects assigned to the current user
    …and whenever a project disappears from that list, mark it as
    deactivated in Hamster by prefixing its tag with 'xx '

--- togglprojects2hamstertags.py
+++ togglprojects2hamstertags.py
@@ -7,9 +7,11 @@ from dataclasses import dataclass
 
 import requests
 
-TOGGL_PROJECTS_URL = 'https://api.track.toggl.com/api/v8/workspaces/{workspace_id}/projects'
+#TOGGL_PROJECTS_URL = 'https://api.track.toggl.com/api/v8/workspaces/{workspace_id}/projects'
+TOGGL_PROJECTS_URL = 'https://api.track.toggl.com/api/v9/me/projects'
 TOGGL_CLIENTS_URL = 'https://api.track.toggl.com/api/v8/workspaces/{workspace_id}/clients'
 CONFIG_FILE = 'hamster2toggl.config'
+DEACTIVATED_PREFIX = 'xx '
 
 config_path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -35,18 +37,16 @@ for client in response.json():
 
 time.sleep(1.25)
 
-url = TOGGL_PROJECTS_URL.format(workspace_id=config['toggl_workspace_id'])
-response = requests.get(url=url, auth=requests_auth)
+response = requests.get(url=projects_url, auth=requests_auth)
 response.raise_for_status()
 # special tag format: `tp::project name (client name)::project id`
 project_tags_by_id = {}
 for project in response.json():
-    try:
-        client_id = project['cid']
-    except KeyError:
+    client_id = project.get('cid')
+    if not client_id:
         client_name = None
     else:
-        client_name= client_id_to_name[client_id]
+        client_name = client_id_to_name[client_id]
     project_tag = 'tp::{project_name} ({client_name})::{project_id}'.format(
         project_name=project['name'],
         client_name=client_name,
@@ -63,7 +63,7 @@ db_connection = sqlite3.connect(config['hamster_db'])
 db_cursor = db_connection.cursor()
 db_cursor.execute(
     'select id, name from tags where name like "tp::%::%" '
-    'or name like "xx tp::%::%"'  # manually deactivated tags
+    f'or name like "{DEACTIVATED_PREFIX} tp::%::%"'  # deactivated tags
 )
 toggl_project_id_to_hamster_tag = {}
 for row in db_cursor:
@@ -83,7 +83,7 @@ update_count = 0
 insert_count = 0
 for toggl_id, tag_name in project_tags_by_id.items():
     try:
-        existing_hamster_tag = toggl_project_id_to_hamster_tag[toggl_id]
+        existing_hamster_tag = toggl_project_id_to_hamster_tag.pop(toggl_id)
     except KeyError:
         existing_hamster_tag = None
     else:
@@ -93,14 +93,22 @@ for toggl_id, tag_name in project_tags_by_id.items():
             continue
 
     if existing_hamster_tag:
+        '''
+        % we probably don't need to worry about manual deactivation anymore
+        % now that we only pull in projects to which the current user is
+        % assigned, assuming that someone else has handled removing the user
+        % from whatever projects are "deactivated"
+
         # don't update manually deactivated tags
-        if existing_hamster_tag.name.startswith('xx '):
+        if existing_hamster_tag.name.startswith(DEACTIVATED_PREFIX):
             print(
                 f'Existing tag "{existing_hamster_tag.name}" is disabled; '
                 f'not overwriting with "{tag_name}"'
             )
             no_change_count += 1
             continue
+        '''
+
         # update
         db_cursor.execute(
             'update tags set name = ? where id = ?',
@@ -120,8 +128,22 @@ for toggl_id, tag_name in project_tags_by_id.items():
         insert_count += 1
         print(f'Inserted new tag "{tag_name}"')
 
-    # TODO: delete hamster tags, matching the special format, that no longer
-    # exist in toggl?
+# add a prefix to hamster tags, matching the special format, that no longer
+# exist in toggl
+for existing_hamster_tag in toggl_project_id_to_hamster_tag.values():
+    if existing_hamster_tag.name.startswith(DEACTIVATED_PREFIX):
+        pass
+    tag_name = DEACTIVATED_PREFIX + existing_hamster_tag.name
+    db_cursor.execute(
+        'update tags set name = ? where id = ?',
+        (tag_name, existing_hamster_tag.pk),
+    )
+    update_count += 1
+    print(
+        f'Updated existing tag "{existing_hamster_tag.name}" to '
+        f'"{tag_name}"'
+    )
+
 
 db_connection.commit()
 print()
